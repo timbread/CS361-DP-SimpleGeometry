88896bd5e72422f9b0667546905f371f3e28c0f0
Sasha Shundrikova <tim.bread@yandex.ru>
2017-03-03 00:06:51 +0300

    Add Squre object to list

--- SimpleGeometry/SimpleGeometry/Program.cs
+++ SimpleGeometry/SimpleGeometry/Program.cs
@@ -16,6 +16,7 @@ namespace SimpleGeometry
             shapes.Add(new Circle(1));
             shapes.Add(new Triangle(6, 5, 4));
             shapes.Add(new RightAngledTriangle(3, 4));
+            shapes.Add(new Square(5));
 
             foreach (Shape shape in shapes)
                 ShapePrinter.Print(shape);
